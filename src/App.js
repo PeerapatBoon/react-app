9e6bb4a1b708aec7cb8f3eea07a2e80830a8bd5e
PeerapatBoon <Peerapat_boo@hotmail.com>
2021-06-01 22:36:29 +0700

    Learn setState lesson

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-const App = () => {
-  const myPic = 'http://lorempixel.com/400/200/nature';
+class App extends Component {
+  state = {
+    user: "Jeerawuth",
+    password : "thisIsAbook",
+    address:{
+      houseNo:123,
+      province: "กรุงเทพฯ"
+
+    }
+  };
+  userClick = () => {
+    this.setState({
+      user:"Ronaldo",
+      password: "IloveyouMoretoday123",
+      address:{
+        houseNo:123,
+        province: "นนทบุรี"
+      }
+    });
+  }
+render(){
   return(
-    <div class="container">
-      <img class="img-thumnail" src={ myPic }
-      alt="nature"/>
+    <div className="container">
+      <div className="row">
+        <div className="col-12 col-sm-6 mx-auto mt-5">
+          <div className="card ml-5 mr-5">
+            <div className="card-header text-right">
+              <button className="btn btn-success btn-sm" onClick = {this.userClick}>
+                คลิกหน่อยนะ
+              </button>
+            </div>
+            <div className="card-body">
+              user: <span className = "ml-2">{this.state.user}</span><br/>
+              password:<span className="ml-2">{this.state.password}</span><br/>
+              address:<span className="ml-2">{this.state.address.province}</span>
+              <span className="ml-1" >{this.state.address.houseNo}</span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
+  }
 }
 export default App;
